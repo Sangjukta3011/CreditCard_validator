f24ad0f5b58cfea21d401e836a35ad659571d175
Sangjukta30 <sanjuktasharma18@gmail.com>
2022-11-29 23:12:39 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sum_odd_digits = 0
+sum_even_digits = 0
+total = 0
+
+#accepting user input
+
+card_number=input("Enter a credit card :")
+card_number = card_number.replace("-","")
+card_number = card_number.replace(" ","")
+card_number= card_number[::-1]
+
+
+for x in card_number[::2]:
+    sum_odd_digits+=int(x)
+for x in card_number[1::2]:
+    x = int(x)*2
+    if x>10:
+        sum_even_digits+=(1+(x%10))
+    else:
+        sum_even_digits+=x
+
+
+
+
+total=sum_even_digits+sum_odd_digits
+
+if total%10 ==0:
+    print("The given credit card number is VALID")
+else :
+    print("The given credit card number is INVALID")
